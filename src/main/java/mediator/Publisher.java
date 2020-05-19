8edbb86eceee320bdc08f0de120e77a9d6eb1ee7
pang623 <pang623j@gmail.com>
2020-05-19 16:33:31 +0800

    add new file

--- src/main/java/mediator/Publisher.java
+++ src/main/java/mediator/Publisher.java
@@ -13,9 +13,9 @@ public class Publisher implements Publishable<ChatData> {
     // Create a collection object that keeps the channel names and their
     // list of observers/subcribers. [Suggestion: Use ArrayList to keep the
     // list of observers. Use TreeMap to map channel name (String type) to the
-    // ArrayList of observers/subcribers.
+    // ArrayList of observers/subcribers. hello
 
-    
+
     @Override
     public void joinChannel(String channelName, Observer<ChatData> observer) {
         // Handle channel join request by the observer/subcriber.
@@ -28,7 +28,7 @@ public class Publisher implements Publishable<ChatData> {
         //   4) If no, then add the observer into the Array of observers.
         //      Otherwise do nothing.
         //   5) Update all observers about their status. E.g., who is already
-        //      in the channel or who has just joined the channel. 
+        //      in the channel or who has just joined the channel.
     }
 
     @Override
@@ -37,7 +37,7 @@ public class Publisher implements Publishable<ChatData> {
         // Requirements:
         //   1) Get the ArrayList of observers/subcribers from the collection
         //      using channel name as the key.
-        //   2) If the observer/subscriber was in the list, remove it. 
+        //   2) If the observer/subscriber was in the list, remove it.
         //      Otherwise just ignore and return immediately.
         //   3) If the ArrayList of observers/subcribers is empty, then
         //      remove the <key, value> pairs from the collection.
